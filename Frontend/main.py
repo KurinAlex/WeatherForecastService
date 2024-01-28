3a83aee325e409c7292400ca6922b91273f36eee
Vladislav <salukvladislav81@gmail.com>
2024-01-28 17:39:09 +0200

    Adding ingex.html

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def forecast_page():
-    return "Hello this is the forecast page"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
