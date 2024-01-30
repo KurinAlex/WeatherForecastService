c061288596e282ebe542391d22977af6de607a3c
Vladislav <salukvladislav81@gmail.com>
2024-01-30 17:16:09 +0200

    Adding graphic

--- Frontend/main.py
+++ Frontend/main.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def forecast_page():
     return render_template("home.html")
 
+@app.route("/graphic")
+def graphic_page():
+    return render_template("graphic.html")
+
+
 if __name__ == "__main__":
     app.run()
