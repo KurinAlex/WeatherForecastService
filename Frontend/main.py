af78ca205b8bb094d426609f9e12dd34f1c0defa
Vladislav <salukvladislav81@gmail.com>
2024-02-01 12:45:08 +0200

    Animated graphic

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
@@ -9,7 +10,10 @@ def forecast_page():
 
 @app.route("/graphic")
 def graphic_page():
-    return render_template("graphic.html")
+    country = request.args.getlist("country")
+    start = request.args.getlist("start")
+    end = request.args.getlist("end")
+    return render_template("graphic.html", country=country, start=start, end=end)
 
 
 if __name__ == "__main__":
